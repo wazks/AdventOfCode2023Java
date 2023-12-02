8ed5c843acd5ebb91a21818d43e2394f1062a28b
Wazks <wazkok@op.pl>
2023-12-02 12:19:37 +0100

    added a missing semilocon in default files

--- day13/Part2.java
+++ day13/Part2.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-
 class Part1 {
     public static void main(String[] args) {
         InputReader inputReader = new InputReader("./input.txt");
 
         while (true) {
             String line = inputReader.readLine();
-            if (line == null) break
+            if (line == null)
+                break;
         }
     }
 }
