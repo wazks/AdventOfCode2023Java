9a6186df4b83ede27ba00081bf639de76a1f9d72
Wazks <tomasz.cieplinski@onet.pl>
2023-12-08 17:47:07 +0100

    day8 complete

--- day08/Part1.java
+++ day08/Part1.java
@@ -1,11 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Part1 {
     public static void main(String[] args) {
         InputReader inputReader = new InputReader("./input.txt");
 
+        Map<String, String[]> nodes = new HashMap<>();
+
+        char[] directions = inputReader.readLine().toCharArray();
+
         while (true) {
             String line = inputReader.readLine();
+
             if (line == null)
                 break;
+
+            if (line.isEmpty())
+                continue;
+
+            nodes.putIfAbsent(line.substring(0, 3),
+                    new String[] { line.substring(7, 10), line.substring(12, 15) });
         }
+
+        String curNode = "AAA";
+        int count = 0;
+        while (!curNode.equals("ZZZ")) {
+            curNode = nodes.get(curNode)[directions[count % directions.length] == 'L' ? 0 : 1];
+            count++;
+        }
+        System.out.println(count);
     }
 }
