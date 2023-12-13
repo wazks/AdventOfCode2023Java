7d6d7335d80c1bad0cdd6cfcd7fd4c6fa0ac39b8
Wazks <tomasz.cieplinski@onet.pl>
2023-12-13 23:47:40 +0100

    day 11 complete

--- day11/Part2.java
+++ day11/Part2.java
@@ -1,11 +1,17 @@
-class Part1 {
+class Part2 {
     public static void main(String[] args) {
-        InputReader inputReader = new InputReader("./input.txt");
+        InputReader inputReader = new InputReader("./inpute.txt");
 
+        Universe universe = new Universe();
         while (true) {
             String line = inputReader.readLine();
             if (line == null)
                 break;
+
+            universe.addRow(line);
         }
+
+        universe.expandSpace(1000000);
+        universe.printSumOfShortestPaths();
     }
 }
